f4896b70b0c1775969f05f61928afa6d0031b385
Nick Haupka <nick.haupka@outlook.de>
2024-11-06 15:51:08 +0100

    update schema

--- classifier/create_table_oal_doctypes.sql
+++ classifier/create_table_oal_doctypes.sql
@@ -1,4 +1,4 @@
-create table unignhaupka.classification_article_reviews_august_2024 (
+create table kb_project_openbib.classification_article_reviews_october_2024 (
 	openalex_id text,
 	doi text,
 	is_research boolean,
